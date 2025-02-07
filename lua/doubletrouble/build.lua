dda718b86df9e91d56093282ac051d3b559b2c49
Bartek Mucha <muchzill4@gmail.com>
2025-02-07 20:01:42 +0000

    Add ghostty

--- lua/doubletrouble/build.lua
+++ lua/doubletrouble/build.lua
@@ -143,6 +143,35 @@ function M.fzf()
   write_file("dist/fzf/fzf.txt", render(template, colors))
 end
 
+function M.ghostty()
+  local template = [[
+cursor-color = $green
+cursor-text = $bg
+
+foreground = $fg
+background = $bg
+
+palette = 0=$black
+palette = 1=$red
+palette = 2=$green
+palette = 3=$yellow
+palette = 4=$blue
+palette = 5=$purple
+palette = 6=$cyan
+palette = 7=$white
+palette = 8=$black
+palette = 9=$red
+palette = 10=$green
+palette = 11=$yellow
+palette = 12=$blue
+palette = 13=$purple
+palette = 14=$cyan
+palette = 15=$white
+]]
+
+  write_file("dist/ghostty/doubletrouble", render(template, colors))
+end
+
 function M.all()
   M.vim()
   M.kitty()
@@ -150,6 +179,7 @@ function M.all()
   M.blink()
   M.slack()
   M.fzf()
+  M.ghostty()
 end
 
 return M
